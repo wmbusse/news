d4478e98f9207822295a730c7fdda31df9ca4da1
William Busse <wmbusse@gmail.com>
2020-07-24 11:52:06 -0500

    newspaper initial commit'

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age','email','first_name','last_name',)
+        
+        
+class CustomUserChangeForm(UserChangeForm):
+
+        class Meta:
+            model = CustomUser
+            fields = ('username', 'email', 'age','first_name','last_name',)
